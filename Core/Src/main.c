60ab4468a5b6002e414cf460fd011e239538e58e
Lautaro <lautaromorelli99@gmail.com>
2023-12-10 16:17:02 -0300

    Add library for calculus

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -129,7 +129,7 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-  SystemInit(); 
+
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
